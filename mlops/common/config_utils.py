0b2fba4c8358109eb06db9bbd826887abd04cb61
sbaidachni <sbaydach@microsoft.com>
2024-02-20 07:19:57 -0800

    Custom skills deployment (#20)
    * slot deployment
    
    * apply black formatting rules
    
    * better requirements
    
    * slot parameter
    
    ---------
    
    Co-authored-by: Ragan <ragan.gum@microsoft.com>

--- mlops/common/config_utils.py
+++ mlops/common/config_utils.py
@@ -1,4 +1,5 @@
 """Configuration utils to load config from yaml/json."""
+
 import os
 from typing import Dict, Any
 from pathlib import Path
@@ -6,17 +7,19 @@ from dotenv import load_dotenv
 import yaml
 
 
-class MLOpsConfig():
+class MLOpsConfig:
     """MLopsConfig Class."""
 
     _raw_config: Any
 
-    def __init__(self, environment: str = 'pr', config_path: Path = 'config/config.yaml'):
+    def __init__(
+        self, environment: str = "pr", config_path: Path = "config/config.yaml"
+    ):
         """Intialize MLConfig with yaml config data."""
         self.config_path = config_path
         self._environment = environment
         load_dotenv()
-        with open(config_path, 'r', encoding='utf-8') as stream:
+        with open(config_path, "r", encoding="utf-8") as stream:
             self._raw_config = yaml.safe_load(os.path.expandvars(stream.read()))
 
     def __getattr__(self, __name: str) -> Any:
@@ -25,7 +28,7 @@ class MLOpsConfig():
 
     def get_flow_config(self, flow_name: str) -> Dict:
         """Get the pipeline configuration for given flow name and environment."""
-        flowconfig_name = f'{flow_name}_{self._environment}'
+        flowconfig_name = f"{flow_name}_{self._environment}"
         if flowconfig_name in self.flow_configs:
             return self.flow_configs[flowconfig_name]
 
